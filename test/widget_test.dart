d1610cb7fb55817572fafd3ccda5f8987a386a3b
maurice Mumbere <mauricendayango@gmail.com>
2025-08-24 20:38:49 +0100

    update import statement to reflect project name change to activite_4

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:activite_3/main.dart';
+import 'package:activite_4/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {

8bbd453a084b6cbd7190312aa5036aeed7664b29
maurice Mumbere <mauricendayango@gmail.com>
2025-08-24 20:38:38 +0100

    refactor main.dart to update home widget and enhance layout with new sections

--- lib/main.dart
+++ lib/main.dart
@@ -12,13 +12,14 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: const MyHomePage(),
+      home: const PageAccueil(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
+// Page d'accueil
+class PageAccueil extends StatelessWidget {
+  const PageAccueil({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -43,14 +44,130 @@ class MyHomePage extends StatelessWidget {
         ],
       ),
       // Body
-      body: Center(child: Image.asset("assets/images/magazin-infos.jpg")),
-      // floatingActionButton
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: Colors.pink,
-        foregroundColor: Colors.white,
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(50)),
-        onPressed: () {},
-        child: const Text('Click'),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Image.asset(
+              "assets/images/magazin-infos.jpg",
+              width: MediaQuery.of(context).size.width,
+              fit: BoxFit.cover,
+            ),
+            PartieTitre(),
+            PartieText(),
+            PartieIcone(),
+            PartieRubrique(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+// Partie titre
+class PartieTitre extends StatelessWidget {
+  const PartieTitre({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(20),
+      width: double.infinity,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: const [
+          Text(
+            "Bienvenue au magazine infos",
+            style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(height: 5),
+          Text(
+            "Votre magazine Numérique, votre source d'inspiration",
+            style: TextStyle(fontSize: 16),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+// Partie texte
+class PartieText extends StatelessWidget {
+  const PartieText({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: const Text(
+        "Magazine infos numérique est bien plus qu'un simple magazine d'informations. C'est votre passerelle vers le monde, une source inestimable de connaissance et d'actualités soigneusement sélectionnées pour vous éclairer sur les enjeux mondiaux, la culture, la science et voir même le divertissement (le jeux).",
+      ),
+    );
+  }
+}
+
+// Partie icone
+class PartieIcone extends StatelessWidget {
+  const PartieIcone({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: [
+        _buildIcone(Icons.phone, 'tel'),
+        _buildIcone(Icons.mail, 'mail'),
+        _buildIcone(Icons.send, 'Partage'),
+      ],
+    );
+  }
+}
+
+Widget _buildIcone(IconData icon, String text) {
+  return Container(
+    padding: const EdgeInsets.all(20),
+    child: Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Icon(icon, size: 40, color: Colors.pink),
+        SizedBox(height: 5),
+        Text(text.toUpperCase(), style: TextStyle(color: Colors.pink)),
+      ],
+    ),
+  );
+}
+
+// Partie rubrique
+class PartieRubrique extends StatelessWidget {
+  const PartieRubrique({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 40),
+      child: Row(
+        children: [
+          Expanded(
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: Image.asset(
+                "assets/images/image-papier.jpg",
+                height: 150,
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          const SizedBox(width: 10),
+          Expanded(
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: Image.asset(
+                "assets/images/image-a-la-une-papier-entete.jpg",
+                height: 150,
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
